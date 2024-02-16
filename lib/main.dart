082b06e4bd22738a389e42080bbadf8385106741
sharmaprashant18 <ps3348644@gmail.com>
2024-02-16 14:12:34 +0545

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:bookapp/view/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-// void main() {
-//   runApp(Home());
-// }
-void main() => runApp(Home());
+void main() {
+  runApp(Home());
+}
 
 class Home extends StatelessWidget {
   @override
